0ca500deac8a441588981108ce645d10b5d50532
Aditya.Priyam <aditya.priyam@ril.com>
2020-08-27 11:06:00 +0530

    unable to read or write data

--- src/shared/constants/RoutePath.js
+++ src/shared/constants/RoutePath.js
@@ -2,5 +2,7 @@ const routePath = {
     DEFAULT: "/",
     FORMS: "/form",
     PESTS_AND_DISEASES: "/pests_diseases",
+    DISPLAY: "/display",
+    INSERT: "/insert",
 };
 export default routePath;
--- src/shared/constants/pests_and_diseases.js
+++ src/shared/constants/pests_and_diseases.js
@@ -1,1 +1,11 @@
-import pests_and_disease from "./pests_and_disease.json";
+// import pests_and_disease from "./pests_and_disease.json";
+const fs = require("fs");
+
+const create = (data) => {
+    fs.writeFileSync("./data.json", String(data), (err) => {
+        if (err) throw err;
+        console.log("file written");
+    });
+};
+
+module.exports = { create };
